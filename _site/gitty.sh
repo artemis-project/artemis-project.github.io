e4041f56845299590fd5e60c39900f74caff8f12
Marquis Kurt <ubunturox104@gmail.com>
2017-04-17 11:53:57 -1000

    Update and test site

---
+++ _site/gitty.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+echo Gitty 1.0
+echo (C) 2017 | Marquis Kurt.
+echo Licensed under GNU GPL v3.
+echo
+echo Beginning commit sequence...
+echo Adding files...
+git add *
+echo Enter your commit message:
+read message
+git commit -m "$message"
+echo Pushing files to website...
+git push
+echo Sequence done. It may take a few moments for Jekyll to build the files properly.
