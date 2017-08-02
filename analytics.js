4c6fe237b59f7ddd8e7c87acd933097badb053b8
Marquis Kurt <ubunturox104@gmail.com>
2017-08-01 21:11:45 -1000

    Update with new post

--- analytics.js
+++
@@ -1,9 +0,0 @@
-//Google Analytics Code
-/* This code is inserted into every webpage via its templates to track which areas are more populated. I respect your privacy and promise to not use this data in such a way that gets advertised to other companies; I am the only one who will use the data to tailor my content accordingly.*/
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-85992968-1', 'auto');
-  ga('send', 'pageview');
